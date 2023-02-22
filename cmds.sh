0a3349e488b2f19462f85c85c6cd341453d272e6
Tech Rapper <104504298+TechRapper@users.noreply.github.com>
2023-02-22 17:41:42 +0530

    Create cmds.sh

---
+++ cmds.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/AOSPA-RMX2030/android_device_realme_r5x -b topaz device/realme/r5x
+
+git clone https://github.com/AOSPA-RMX2030/android_vendor_realme_r5x -b topaz vendor/realme/r5x
+
+git clone https://www.opencode.net/DumprX/realme/r5x -b trinket-user-10-QKQ1.200209.002-release-keys --depth=1 device/realme/r5x/dump
+
+git clone https://github.com/LineageOS/android_tools_extract-utils tools/extract-utils
